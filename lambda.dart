6cfdb61a1e89665213798dc460294993ca4e03db
Pratik Kapadnis <prkapadnis2001@gmail.com>
2021-11-13 16:44:18 +0530

    lambda function

---
+++ lambda.dart
@@ -0,0 +1,9 @@
+main() {
+  // syntax:
+  // [return type] function type(parameter) => exmpression;
+  printmsg();
+  print(add(1, 2));
+}
+
+printmsg() => print('hello world');
+add(a, b) => a + b;
