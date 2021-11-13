49ae6cad0fcdea9cc779db73610e8007d9dfeb6c
Pratik Kapadnis <prkapadnis2001@gmail.com>
2021-11-13 16:42:52 +0530

    OOP

---
+++ OOP/classses.dart
@@ -0,0 +1,17 @@
+class Name {
+  String student = 'Pratik';
+
+  void display() {
+    print(student);
+  }
+}
+
+class Details {
+  String name
+
+}
+
+main() {
+  var obj = new Name();
+  obj.display();
+}
